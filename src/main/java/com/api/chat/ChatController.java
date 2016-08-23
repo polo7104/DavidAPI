5783d69baa9c2631ea9abc65e2e50d497e8adae8
David <godluv81@gmail.com>
2016-08-23 22:32:40 +0900

    ..
    (cherry picked from commit e9a45575371d2af942ee569874f59a630cf2ee98)

--- src/main/java/com/api/chat/ChatController.java
+++ src/main/java/com/api/chat/ChatController.java
@@ -1,6 +1,8 @@
 package com.api.chat;
 
+import com.api.chat.Beans.Friends;
 import com.api.chat.Beans.User;
+import com.api.chat.Repository.FriendsRepository;
 import com.api.chat.Repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-import java.util.Date;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
@@ -27,11 +29,12 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 @CrossOrigin
 public class ChatController {
 
-    @Autowired UserRepository userRepository;
+    @Autowired UserRepository       userRepository;
+    @Autowired FriendsRepository    friendsRepository;
 
 
 
-    @RequestMapping(value = "/singup", method = POST)
+    @RequestMapping(value = "/signup", method = POST,  produces  = "application/json")
     public ResponseEntity signUp(@RequestBody @Valid User user, BindingResult result){
 
         if(result.hasErrors()){
@@ -41,7 +44,6 @@ public class ChatController {
 
         user.setCreate(new Date());
         user.setUpdate(new Date());
-//        user.setFriends(null);
 
         User userResult = userRepository.save(user);
 
@@ -49,10 +51,93 @@ public class ChatController {
         return new ResponseEntity<>(userResult, HttpStatus.CREATED);
     }
 
+    @RequestMapping(value = "/search/{username}", method = GET)
+    public ResponseEntity searchOne(@PathVariable String username){
+
+        if(username == null){
+            return new ResponseEntity<>("Nothing", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = userRepository.findByUsername(username);
+        //
+
+
+        return new ResponseEntity(user, HttpStatus.ACCEPTED);
+    }
+
+    @RequestMapping(value = "/add/{id}/friend/{friend}", method = GET)
+    public ResponseEntity addFriend(@PathVariable("id") Long id, @PathVariable("friend") String str_friend){
+
+        if(id == null || str_friend == null){
+            return new ResponseEntity<>("Nothing", HttpStatus.BAD_REQUEST);
+        }
+
+        Friends friends = new Friends();
+
+        User user = userRepository.findOne(id);
+
+        if(user == null){
+            return new ResponseEntity<>("user is null", HttpStatus.BAD_REQUEST);
+        }
+
+        friends.setUser(user);
+        friends.setFriend(str_friend);
+        friends.setCreate(new Date());
+        friends.setUpdate(new Date());
+
+        Friends result = friendsRepository.save(friends);
+
+        return new ResponseEntity(result, HttpStatus.ACCEPTED);
+    }
+
     @RequestMapping(value = "/get", method = GET)
     public ResponseEntity<List<User>> getAll(){
         List<User> userList= userRepository.findAll();
 
         return new ResponseEntity<List<User>>(userList, HttpStatus.ACCEPTED);
+    }
+
+    @RequestMapping(value = "/store")
+    public ResponseEntity storeOne(){
+        User user = new User();
+
+			user.setUsername("david");
+			user.setPassword("4860ss");
+			user.setCreate(new Date());
+			user.setUpdate(new Date());
+			user.setReg_id("dddsseeessdfdcsdfasds");
+			User stored = userRepository.save(user);
+
+
+        Friends friends = new Friends();
+            friends.setUser(stored);
+            friends.setFriend("aa");
+            friends.setCreate(new Date());
+            friends.setUpdate(new Date());
+            friendsRepository.save(friends);
+
+        Friends friend2 = new Friends();
+        friend2.setUser(stored);
+        friend2.setFriend("bb");
+        friend2.setCreate(new Date());
+        friend2.setUpdate(new Date());
+        friendsRepository.save(friend2);
+
+//        Friends[] friends = new Friends[2];
+//
+//
+//        for(int i=0; i<2; i++) {
+//            friends[i].setUser(stored);
+//            friends[i].setFriend("aa"+i);
+//            friends[i].setFriends_create(new Date());
+//            friends[i].setFriends_update(new Date());
+//            friendsRepository.save(friends[i]);
+//        }
+
+
+        List saved = Arrays.asList(stored, friend2);
+
+
+        return new ResponseEntity<> (saved, HttpStatus.OK);
     }
 }
